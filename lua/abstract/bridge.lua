aee49f2d0a018121631b765d602b951816377881
Ali Shahid <shaeinst@gmail.com>
2024-04-24 12:07:13 +0400

    [fix: map] now user can define and override default mapping

--- lua/abstract/bridge.lua
+++ lua/abstract/bridge.lua
@@ -1,4 +1,5 @@
 local M = {}
+vim.g.MAPPINGS = {}
 
 local function lazy()
 	-- bootstrap lazy.nvim
@@ -21,6 +22,27 @@ local function lazy()
 	require("lazy").setup(opts)
 end
 
+local function loadMap()
+	vim.g.MAPPINGS = vim.tbl_extend(
+		"force",
+		require("abstract.configs.mapping").builtin,
+		require("override.mapping") -- "~/.config/nvim/lua/override/mapping.lua"
+	)
+	local set_map = require("abstract.utils.map").set_map
+	set_map(vim.g.MAPPINGS)
+	vim.api.nvim_create_augroup("AbstractAutoGroup", { clear = true })
+	vim.api.nvim_create_autocmd({ "User" }, {
+		desc = "Set mappings",
+		pattern = "*",
+		group = "AbstractAutoGroup",
+		callback = function(type)
+			if type.match == "AbstractLoadMapping" then
+				set_map(vim.g.MAPPINGS)
+			end
+		end,
+	})
+end
+
 function M.setup()
 	-- call abstract autocmds
 	require("abstract.configs.autocmd")
@@ -28,10 +50,11 @@ function M.setup()
 	require("abstract.configs.vimopt")
 	-- Override Abstract's default with user config
 	dofile(vim.fn.stdpath("config") .. "/init.lua")
+	-- Even though we are using a plugin for mapping, and loadMap() should be loaded after initializing lazy,
+	-- we are not doing so. I think lazy resets the global variable or something (honestly, I don't know),
+	-- and we are calling loadMap before lazy
+	loadMap()
 	lazy()
-	require("abstract.utils.map").set_map(require("abstract.configs.mapping").builtin)
-	-- Override Abstract's plugin mappings with user-defined ones
-	require("abstract.utils.map").set_map(require("override.mapping"))
 end
 
 return M
--- lua/abstract/utils/map.lua
+++ lua/abstract/utils/map.lua
@@ -1,13 +1,16 @@
 local M = {}
-local wk = require("which-key")
 
 function M.set_plugin(name)
-	local key = require("abstract.configs.mapping").plugin[name]
-	wk.register(key)
+	local mapping = require("abstract.configs.mapping").plugin[name]
+	vim.g.MAPPINGS = vim.tbl_extend("keep", vim.g.MAPPINGS, mapping)
+	vim.api.nvim_command("doautocmd User AbstractLoadMapping")
 end
 
 function M.set_map(map)
-	wk.register(map)
+	local import, wk = pcall(require, "which-key")
+	if import then
+		wk.register(map)
+	end
 end
 
 return M
