b96b454b9000fc39d239a8df58cf3a32083fb6e3
Ali Shahid <shaeinst@gmail.com>
2024-08-03 16:57:03 +0400

    [refactor] code format

--- lua/abstract/plugins/dart-vim-plugin.lua
+++ lua/abstract/plugins/dart-vim-plugin.lua
@@ -1,22 +1,28 @@
--- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
--- ───────────────────────────────────────────────── --
---   Plugin:    dart-vim-plugin
---   Github:    github.com/dart-lang/dart-vim-plugin
--- ───────────────────────────────────────────────── --
--- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+--[[
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+────────────────────────────────────────────────
+Plugin:    dart-vim-plugin
+Github:    https://github.com/dart-lang/dart-vim-plugin
 
+dart-vim-plugin provides filetype detection, syntax highlighting,
+and indentation for Dart code in Vim.
+────────────────────────────────────────────────
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+--]]
 
-
-
-return {
-	"dart-lang/dart-vim-plugin", -- Syntax highlighting for Dart in Vim
-	ft = {"dart"},
-	config = function()
-		-- Enable HTML syntax highlighting inside Dart strings  (default: false)
-		vim.g.dart_html_in_string = "v.true"
-		-- Enable Dart style guide syntax (like 2-space indentation)
-		vim.g.dart_style_guide = 2
-		-- Enable DartFmt execution on buffer save with
-		vim.g.dart_format_on_save = 0
-	end,
+local spec = {
+	"dart-lang/dart-vim-plugin",
+	lazy = true,
+	ft = { "dart" },
 }
+
+spec.config = function()
+	-- Enable HTML syntax highlighting inside Dart strings  (default: false)
+	vim.g.dart_html_in_string = "v.true"
+	-- Enable Dart style guide syntax (like 2-space indentation)
+	vim.g.dart_style_guide = 2
+	-- Enable DartFmt execution on buffer save with
+	vim.g.dart_format_on_save = 0
+end
+
+return spec
--- lua/abstract/plugins/flutter-tools.lua
+++ lua/abstract/plugins/flutter-tools.lua
@@ -1,11 +1,13 @@
---[[━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+--[[
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ────────────────────────────────────────────────
 Plugin:    flutter-tools.nvim
 Github:    https://github.com/akinsho/flutter-tools.nvim
 
 Tools to help create flutter apps in neovim using the native lsp
 ────────────────────────────────────────────────
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━]]
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+--]]
 
 local spec = {
 	"akinsho/flutter-tools.nvim",
