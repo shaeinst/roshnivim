d92b9fb2d375ae074881ea1ea78fad750eaff590
Ali Shahid <shaeinst@gmail.com>
2024-08-29 22:57:36 +0400

    [mapping] add maps for trouble

--- lua/abstract/configs/mapping.lua
+++ lua/abstract/configs/mapping.lua
@@ -139,7 +139,13 @@ M.plugin = {
 	},
 
 	["folke/trouble.nvim"] = {
-		{ "<Leader>t", "<CMD>Trouble diagnostics toggle<CR>", desc = "Diagnostics (Trouble)" },
+		{ "<leader>T",  group = "Trouble" },
+		{ "<leader>t",  "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",           desc = "Buffer Diagnostics (Trouble)" },
+		{ "<Leader>Tt", "<CMD>Trouble diagnostics toggle<CR>",                        desc = "Diagnostics (Trouble)" },
+		{ "<leader>Ts", "<cmd>Trouble symbols toggle focus=false<cr>",                desc = "Symbols (Trouble)" },
+		{ "<leader>Tl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)" },
+		{ "<leader>TL", "<cmd>Trouble loclist toggle<cr>",                            desc = "Location List (Trouble)" },
+		{ "<leader>Tq", "<cmd>Trouble qflist toggle<cr>",                             desc = "Quickfix List (Trouble)" },
 	},
 
 	["nvim-neo-tree/neo-tree.nvim"] = {
