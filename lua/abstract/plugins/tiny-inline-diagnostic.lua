271b492307501cab1903b9711200b96255a36205
Ali Shahid <shaeinst@gmail.com>
2024-08-29 23:05:55 +0400

    [plugins/tiny-inline-diagnostic] upgrade to latest config

--- lua/abstract/plugins/tiny-inline-diagnostic.lua
+++ lua/abstract/plugins/tiny-inline-diagnostic.lua
@@ -39,6 +39,9 @@ spec.config = function()
 			factor = 0.27,
 		},
 		options = {
+			-- Show the source of the diagnostic.
+			show_source = false,
+
 			-- Throttle the update of the diagnostic when moving cursor, in milliseconds.
 			-- You can increase it if you have performance issues.
 			-- Or set it to 0 to have better visuals.
@@ -50,12 +53,24 @@ spec.config = function()
 			-- If multiple diagnostics are under the cursor, display all of them.
 			multiple_diag_under_cursor = false,
 
+			-- Enable diagnostic message on all lines.
+			-- /!\ Still an experimental feature, can be slow on big files.
+			multilines = false,
+
 			overflow = {
 				-- Manage the overflow of the message.
 				--    - wrap: when the message is too long, it is then displayed on multiple lines.
-				--    - none: the message will not be truncated, and will be displayed on a single line.
-				mode = "none",
+				--    - none: the message will not be truncated.
+				--    - oneline: message will be displayed entirely on one line.
+				mode = "oneline",
 			},
+
+			-- Format the diagnostic message.
+			-- Example:
+			-- format = function(diagnostic)
+			--     return diagnostic.message .. " [" .. diagnostic.source .. "]"
+			-- end,
+			format = nil,
 
 			--- Enable it if you want to always have message with `after` characters length.
 			break_line = {
@@ -65,6 +80,14 @@ spec.config = function()
 
 			virt_texts = {
 				priority = 2048,
+			},
+
+			-- Filter by severity.
+			severity = {
+				vim.diagnostic.severity.ERROR,
+				vim.diagnostic.severity.WARN,
+				vim.diagnostic.severity.INFO,
+				vim.diagnostic.severity.HINT,
 			},
 		},
 	})
