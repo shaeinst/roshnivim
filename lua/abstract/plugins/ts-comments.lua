dc8b92d2ec42a8181d0b642f5396d4fb5e0107b5
Ali Shahid <shaeinst@gmail.com>
2024-12-28 23:28:15 +0400

    [new plugin] add ts-comments.nvim configs

---
+++ lua/abstract/plugins/ts-comments.lua
@@ -0,0 +1,18 @@
+--[[
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+─────────────────────────────────────────────────
+Plugin: ts-comments.nvim
+Github: github.com/folke/ts-comments.nvim
+
+Tiny plugin to enhance Neovim's native comments
+─────────────────────────────────────────────────
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+--]]
+
+local spec = {
+	"folke/ts-comments.nvim",
+	opts = {},
+	event = "VeryLazy",
+}
+
+return spec
