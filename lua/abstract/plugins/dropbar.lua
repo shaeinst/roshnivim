fc0a5087c7f75f27c0fd0bea8f1e5558a637436d
Ali Shahid <shaeinst@gmail.com>
2024-12-29 19:44:09 +0400

    [new plugin] add dropbar.nvim

---
+++ lua/abstract/plugins/dropbar.lua
@@ -0,0 +1,21 @@
+--[[
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+─────────────────────────────────────────────────
+Plugin: dropbar.nvim
+Github: https://github.com/Bekaboo/dropbar.nvim
+
+IDE-like breadcrumbs, out of the box
+─────────────────────────────────────────────────
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+--]]
+
+local spec = {
+	"Bekaboo/dropbar.nvim",
+	lazy = true,
+}
+
+spec.setup = function()
+	--
+end
+
+return spec
